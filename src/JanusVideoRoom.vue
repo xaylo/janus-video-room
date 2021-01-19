16a2b7b300a15725263c4d4e3f3498ed09df2973
Alex Jay <alex@jaythegeek.io>
2021-01-19 15:20:36 +0100

    fixes

--- src/JanusVideoRoom.vue
+++ src/JanusVideoRoom.vue
@@ -310,7 +310,8 @@ import { screenMixin } from "./mixins/screen";
 import { settingsMixin } from "./mixins/settings";
 
 import ElectronScreenSelector from "./electron/ScreenSelector";
-const axios = require("axios");
+import axios from "axios";
+
 export default {
   props: {
     roomData: {
